f5efbb318144c30cc11f397ff3ed81faa18d7968
audibleblink <virtualsnyper@gmail.com>
2014-12-14 13:40:06 -0500

    annotate currying playground

--- currying.playground/section-1.swift
+++ currying.playground/section-1.swift
@@ -1,22 +1,21 @@
-// Playground - noun: a place where people can play
-
-import UIKit
-
+// Standard way of writing a greet function
 func greet(name: String, salutation: String) -> String {
     return "\(salutation) \(name)"
 }
 
 greet("alex", "hello")
 
-func greeter(salutation: String) -> (String) -> String {
-    
-    func greet(name: String) -> String {
-        return "\(salutation) \(name)"
+
+// Curried function
+func greeter(salutation: String) -> (String) -> String { // greeter returns a function that takes a string. That returned function then returns a String
+
+    func greet(name: String) -> String { // build the function that is to be returned
+        return "\(salutation) \(name)" // name will be provided when this function is called later; outside of this greeter function
     }
-    
-    return greet //returns a function with the salutation prepopulated
+
+    return greet //returns a function with the salutation prepopulated, takes a String; return a String
 }
 
 let greetByName = greeter("Hello there")
 
-greetByName("Alex")
+println( greetByName("Alex") )
